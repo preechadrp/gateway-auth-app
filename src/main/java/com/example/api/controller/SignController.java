96446ad161bdb2920209ab7846ab6d4b41065a8f
H P <H P@LAPTOP-KASON>
2026-05-19 06:42:23 +0700

    update

--- src/main/java/com/example/api/controller/SignController.java
+++ src/main/java/com/example/api/controller/SignController.java
@@ -4,13 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//SignController ไม่จำเป็นต้องมีแต่ทำไว้เพื่อ demo เท่านั้น เพราะระบบจะวิ่งไปที่ .yml อัตโนมัติถ้าไม่มีการสร้าง controller ไว้
+
 @RestController
 @RequestMapping("/api/sign")
 public class SignController {
 
 	@GetMapping
 	public String sign() {
-
 		return "SIGN API OK";
 	}
 }
