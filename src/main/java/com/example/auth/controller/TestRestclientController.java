3b02cf8eadd054811165a57951aac417e7e53e29
H P <H P@LAPTOP-KASON>
2026-05-23 19:33:21 +0700

    update

--- src/main/java/com/example/auth/controller/TestRestclientController.java
+++ src/main/java/com/example/auth/controller/TestRestclientController.java
@@ -3,11 +3,15 @@ package com.example.auth.controller;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClient;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping("/auth")
 public class TestRestclientController {
@@ -22,13 +26,19 @@ public class TestRestclientController {
 	public String test() {
 		String testurl = "http://localhost:8080/auth/login";
 
-		String response = this.restClient.post()
+		ResponseEntity<String> response = restClient.post()
 				.uri(URI.create(testurl))
+				//.body(jsonBody)
 				.retrieve()
-				.body(String.class);
+				.toEntity(String.class);
 
-		if (response != null) {
-			return response;
+		int status = response.getStatusCode().value();
+		String body = response.getBody();
+		log.info("status : {}", status);
+		log.info("body : {}", body);
+
+		if (body != null) {
+			return body;
 		}
 
 		return "TestRestclient Failed";
